b874ced12146746db34405da280e02124d3b39d2
toilaphuoc <muontendailam@gmail.com>
2026-02-03 20:19:02 +0700

    Add files via upload

---
+++ Lec8_ObjectAndClass/Contructor/Example1/Bike.java
@@ -0,0 +1,13 @@
+package lec8_ObjectandClasses.Constructor.Example1;
+//Contructor khong co tham so
+public class Bike {
+
+    Bike() {
+        System.out.println("Bike is created");
+    }
+
+    public static void main(String args[]) {
+        Bike b = new Bike();
+    }
+
+}
