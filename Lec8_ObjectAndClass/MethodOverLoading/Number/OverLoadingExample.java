3a4fbcf59050ef94784e70f3ae1f4670cd0175e6
toilaphuoc <muontendailam@gmail.com>
2026-02-03 20:20:02 +0700

    Add files via upload

---
+++ Lec8_ObjectAndClass/MethodOverLoading/Number/OverLoadingExample.java
@@ -0,0 +1,13 @@
+package lec8_ObjectandClasses.MethodOverloading.Number;
+//Overloading theo number(so luong tham so)
+public class OverLoadingExample {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+}
---
+++ Lec8_ObjectAndClass/MethodOverLoading/Number/Processor.java
@@ -0,0 +1,9 @@
+package lec8_ObjectandClasses.MethodOverloading.Number;
+
+public class Processor {
+
+    public static void main(String[] args) {
+        System.out.println(OverLoadingExample.add(1, 2));
+        System.out.println(OverLoadingExample.add(1, 2, 3));
+    }
+}
