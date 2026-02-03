930dfb1fa0e5fc4c12c4d55dc842308b4c3ba1eb
toilaphuoc <muontendailam@gmail.com>
2026-02-03 20:06:52 +0700

    Add files via upload

---
+++ Lec9_InertanceAndPolymarphism/Example2/Animal.java
@@ -0,0 +1,12 @@
+
+package Lec9_InertanceAndPolymarphism.Example2;
+
+
+public class Animal {
+    String source = "VietNam";
+
+    void makeSound() {
+        System.out.println("Make a sound");
+    }
+
+}
