b2e5d9512dd5a72c1eb143c43a769c170066c5ce
toilaphuoc <muontendailam@gmail.com>
2026-02-03 20:38:35 +0700

    Add files via upload

---
+++ Lec11_Encapsulation/Final/Method/Bike.java
@@ -0,0 +1,21 @@
+
+package Lec11_Encapsulation.Final.Method;
+
+
+class Bike {
+    final void run() {
+        System.out.println("running");
+    }
+}
+
+class Honda extends Bike {
+
+    void run() {  
+        System.out.println("running safely with 100kmph");
+    }
+
+    public static void main(String args[]) {
+        Honda honda = new Honda();
+        honda.run();
+    }
+}
