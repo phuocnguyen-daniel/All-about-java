df08e8ad6eb33fecb1cfad0ccc9ac430e0134b4f
toilaphuoc <muontendailam@gmail.com>
2026-02-03 20:40:05 +0700

    Add files via upload

---
+++ Lec11_Encapsulation/Static/Variable/Person.java
@@ -0,0 +1,17 @@
+
+package Lec11_Encapsulation.Static.Variable;
+
+
+public class Person {
+    private String name ;
+    private static int count = 0;
+    public Person(String n){
+        name = n;
+        count++;
+        System.out.println(count);
+    }
+    public static void main(String[] args) {
+        Person p1 = new Person("Trang");
+        Person p2 = new Person("Trinh");
+    }
+}
