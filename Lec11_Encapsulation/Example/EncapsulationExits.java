388e64d75bf22d2bf8546f43e1699b8292b0e29f
toilaphuoc <muontendailam@gmail.com>
2026-02-03 20:37:00 +0700

    Add files via upload

---
+++ Lec11_Encapsulation/Example/EncapsulationExits.java
@@ -0,0 +1,11 @@
+
+package Lec11_Encapsulation.Example;
+
+
+public class EncapsulationExits {
+    public static void main(String[] args) {
+        Person a = new Person();
+        a.name="Trang";
+        a.setWeight(-10);
+    }
+}
---
+++ Lec11_Encapsulation/Example/Person.java
@@ -0,0 +1,13 @@
+
+package Lec11_Encapsulation.Example;
+
+public class Person {
+    public String name;
+    private double weight;
+    public void setWeight(double weight){
+        if(this.weight<0)
+            System.out.println("Weigth cannot negative");
+        else
+            this.weight = weight;
+    }
+}
