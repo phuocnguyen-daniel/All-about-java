2062ae7d8cadca3f2618d032206244c95bd05390
toilaphuoc <muontendailam@gmail.com>
2026-02-03 20:19:40 +0700

    Add files via upload

---
+++ Lec8_ObjectAndClass/MethodOverLoading/DataType/OverLoadingExample2.java
@@ -0,0 +1,13 @@
+package lec8_ObjectandClasses.MethodOverloading.DataType;
+//Overloading theo Datatype (kieu du lieu)
+public class OverLoadingExample2 {
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+}
---
+++ Lec8_ObjectAndClass/MethodOverLoading/DataType/Processor.java
@@ -0,0 +1,9 @@
+package lec8_ObjectandClasses.MethodOverloading.DataType;
+
+public class Processor {
+
+    public static void main(String[] args) {
+        System.out.println(OverLoadingExample2.multiply(1, 2));   
+        System.out.println(OverLoadingExample2.multiply(1.5, 2)); 
+    }
+}
